c402577cfd4a270c40932003ef4d0a0f51838f5d
Ze40 <miko.r4r@gmail.com>
2025-04-28 20:56:45 +0300

    chore(server): add server from prev version

---
+++ server/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: !(process.env.MODE_ENV !== 'dev'),
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+  ],
+})
+export class AppModule {}
